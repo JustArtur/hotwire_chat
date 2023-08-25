8362a21af70986d4cbcfd3a351ca36f96ded5b32
Artur Kharisov <arturkharisov@Arturs-MacBook-Pro.local>
2023-08-25 02:27:31 -0700

    User must now verify their email before logging in

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,25 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Sending email
+  # config.action_mailer.perform_deliveries = true
+
+  # Email configuration
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => "localhost", :port => 3000 }
+  
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            'arturmonkey45@gmail.com',
+    password:             'htixduvtgomzyaxa',
+    authentication:       'plain',
+    enable_starttls_auto: true,
+    open_timeout:         5,
+    read_timeout:         5 }
 end
